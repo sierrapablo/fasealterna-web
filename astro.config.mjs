58acfc26f482356df386637435e910e0a5d6cf39
rosbeldev <jorgerosbelcruzarias@gmail.com>
2025-07-19 01:46:56 +0200

    feat: create generic Header component with OpenGraph support and native Astro page transitions
    - Created generic Header component in Astro
    - Added support for OpenGraph meta tags
    - Implemented native Astro page transitions
    - Replaced React components with Astro components
    - Installed astro-icons to use icons in Astro instead of React icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
+import icon from "astro-icon";
 import netlify from '@astrojs/netlify';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://example.com",
   vite: {
     plugins: [tailwindcss()],
   },
 
   adapter: netlify(),
-  integrations: [react()],
+  integrations: [react(), icon()],
 });
