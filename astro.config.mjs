e794c2cab0a8a48a83f43178f0a024f887fd72c7
sierrapablo <proyectos@sierrapablo.dev>
2025-07-23 23:06:27 +0200

    🛠️ configure tooling : configure server netlify building

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from "astro-icon";
 import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://fasealterna.es",
   vite: {
     plugins: [tailwindcss()],
   },
-
   adapter: netlify(),
-  integrations: [react(), icon(), sitemap()],
+  integrations: [icon(), sitemap()],
+  env: {
+    schema: {
+      CONCERTS_API_ENDPOINT: envField.string({ context: "server", access: "public" })
+    }
+  }
 });
