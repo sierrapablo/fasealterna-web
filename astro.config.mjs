1cfaf617cf2f002996a8d42a95443f755f134f13
sierrapablo <proyectos@sierrapablo.dev>
2025-07-23 23:01:27 +0200

    📝 update release notes : update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fasealterna.netlify.app",
+  site: "https://fasealterna.es",
   vite: {
     plugins: [tailwindcss()],
   },
